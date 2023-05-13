cd52fa2fa6a477d0de4bf572b33a9d0d330b0c9a
Lucas Sinke <lsinke@student.codam.nl>
2023-05-13 17:30:03 +0200

    fix some stuff, break the rest

--- srcs/requirements/mariadb/tools/launch.sh
+++ srcs/requirements/mariadb/tools/launch.sh
@@ -1,15 +1,18 @@
 if [ ! -f "/data/wordpress_db/mysql_upgrade_info" ]; then
+  echo "Database does not exist, creating..."
   mysql_install_db --defaults-extra-file=/etc/my.cnf.d/mariadb.conf \
                    --user=mysql \
                    --basedir=/usr --datadir=/data/wordpress_db \
                    --skip-test-db
 
+  echo "Database should be created now, setting up accounts..."
   mariadbd --defaults-extra-file=/etc/my.cnf.d/mariadb.conf --user=mysql --bootstrap << EOF
 FLUSH PRIVILEGES;
 
 DELETE FROM	mysql.user WHERE User='';
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD' ;
+
 CREATE DATABASE $MARIADB_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci ;
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PASSWORD' ;
 GRANT ALL ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' ;
@@ -17,4 +20,5 @@ FLUSH PRIVILEGES;
 EOF
 fi
 
+echo "Running mariadb!"
 mariadbd --defaults-extra-file=/etc/my.cnf.d/mariadb.conf --user=mysql --basedir=/usr --datadir=/data/wordpress_db --console
--- srcs/requirements/wordpress/tools/launch.sh
+++ srcs/requirements/wordpress/tools/launch.sh
@@ -1,15 +1,16 @@
 cd /data/wordpress
 
-while ! mariadb --host=$MARIADB_HOST --user=$MARIADB_USER -password=$MARIADB_PASSWORD $MARIADB_DATABASE; do
+while ! mariadb --host=$MARIADB_HOST --user=$MARIADB_USER --password=$MARIADB_PASSWORD $MARIADB_DATABASE; do
+  echo "Database not yet running, retrying..."
   sleep 1
 done
+echo "Connected to database"
 
 if [ ! -f .installed ]; then
+  echo "Wordpress not installed! Installing..."
   /tools/install_wordpress.sh && touch .installed
 fi
 
 rm -f /etc/php8/php-fpm.d/www.conf
 
 php-fpm8 -F
-
-top -b
