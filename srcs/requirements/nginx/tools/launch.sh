483ef2dca1f33ee89e63ff8f9cce649bc1971412
Lucas Sinke <lsinke@student.codam.nl>
2023-05-22 15:40:40 +0200

    Make launch script executable

--- srcs/requirements/nginx/tools/launch.sh
+++ srcs/requirements/nginx/tools/launch.sh
@@ -3,6 +3,7 @@
 if [ "$COMPOSE_PROFILES" != "" ] && [ ! -d /data/wordpress/static ]; then
   mkdir -p /data/wordpress/static
   tar xzf /website.tar.gz --directory /data/wordpress/static
+  chmod 0777 -R /data/wordpress/static
 fi
 
 exec nginx -g "daemon off ;"
