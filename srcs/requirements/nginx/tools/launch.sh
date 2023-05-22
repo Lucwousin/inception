f7a8cffe2a1c5d163c44b2a3f9c634bbae97e0dd
Lucas Sinke <lsinke@student.codam.nl>
2023-05-22 15:22:16 +0200

    Add ancient website from 2012

--- srcs/requirements/nginx/tools/launch.sh
+++ srcs/requirements/nginx/tools/launch.sh
@@ -2,7 +2,7 @@
 
 if [ "$COMPOSE_PROFILES" != "" ] && [ ! -d /data/wordpress/static ]; then
   mkdir -p /data/wordpress/static
-  tar xzf /website.tar.gz /data/wordpress/static/.
+  tar xzf /website.tar.gz --directory /data/wordpress/static
 fi
 
 exec nginx -g "daemon off ;"
